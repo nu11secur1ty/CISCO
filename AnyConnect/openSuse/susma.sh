91e45f4bd6bad64533a54a870c7cc673bf7195e9
nu11secur1ty <venvaropt@gmail.com>
2020-04-03 01:35:35 +0300

    Update susma.sh

--- AnyConnect/openSuse/susma.sh
+++ AnyConnect/openSuse/susma.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 # @nu11secur1ty
-zypper -n install network-manager-openvpn-gnome
-zypper -n install network-manager-vpnc-gnome
-zypper -n install network-manager-openconnect-gnome
+zypper install -y network-manager-openvpn-gnome
+zypper install -y network-manager-vpnc-gnome
+zypper install -y network-manager-openconnect-gnome
 exit 0;
